2195134bf9940a30463b20631d1800a89ef9adf9
Bogdan Kirillov <8k1r1ll0v@gmail.com>
2019-08-04 19:37:22 +0300

    Admin management

--- botik_s_lapkami.py
+++ botik_s_lapkami.py
@@ -19,17 +19,41 @@ bot = telebot.TeleBot(TOKEN)
 mstm = Mystem()
 server = Flask(__name__)
 
+LIFETIME_NOBILITY = {
+    "can_change_info": False,
+    "can_post_messages": False,
+    "can_edit_messages": False,
+    "can_delete_messages": True,
+    "can_invite_users": True,
+    "can_restrict_members": True,
+    "can_pin_messages": True,
+    "can_promote_members": False
+}
+
+NORMAL_USERS = {
+    "can_change_info": False,
+    "can_post_messages": False,
+    "can_edit_messages": False,
+    "can_delete_messages": False,
+    "can_invite_users": False,
+    "can_restrict_members": False,
+    "can_pin_messages": False,
+    "can_promote_members": False
+}
 
 @bot.message_handler(commands=["promote"])
 def grant_a_lifetime_nobility(message):
-    user_name = message
-    print(message)
-   # bot.reply_to(message, "Высочайшим позволением участнику "+user_name+" даровано личное дворянство.")
+    user = message.reply_to_message.from_user
+    bot.promote_chat_member(message.chat.id, user.id, *LIFETIME_NOBILITY)
+    bot.reply_to(
+        message, "Высочайшим позволением участнику "+user.username+" даровано личное дворянство."
+    )
 
 @bot.message_handler(commands=["demote"])
 def revoke_a_lifetime_nobility(message):
-    user_name = message
-    bot.reply_to(message, "Высочайшим указом "+user_name+" лишается личного дворянства.")
+    user = message.reply_to_message.from_user
+    bot.promote_chat_member(message.chat.id, user.id, *NORMAL_USERS)
+    bot.reply_to(message, "Высочайшим указом "+user.username+" лишается личного дворянства.")
     
 def authorised(message):
     """"Check if user is authorised"""
